6ee1e460555ec6957de6198e09ff8e9db5f6091f
siba055 <78475903+siba055@users.noreply.github.com>
2021-02-04 21:20:27 +0530

    assignment week 1

---
+++ Q2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ 
+    float a,b;
+    printf("enter temperature:\n");
+    scanf("%f",&a);
+    b=(a*9/5)+32;
+    printf("temperature is:%f \n",b);
+}
+
+
+
+
+
+    
+
---
+++ Q3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ 
+    float a,b;
+    printf("enter radius:\n");
+    scanf("%f",&a);
+    b=(a*2);
+    printf("diameter is:%f \n",b);
+}
+
+
+
+
+
+    
+
