500983188438f2a4374796d9d646017ba60a0683
siba055 <78475903+siba055@users.noreply.github.com>
2021-02-06 11:25:57 +0530

    Update Q3.c

--- assignment week 1/Q3.c
+++ assignment week 1/Q3.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main()
 { 
-    float a,b;
+    float a,b,circum,area;
     printf("enter radius:\n");
     scanf("%f",&a);
     b=(a*2);
-    printf("diameter is:%f \n",b);
+    circum=(2*3.14)*a;
+    area=(3.14*a)*a;
+    printf("diameter is:%f \n circumference=%f \n area=%f",b,circum,area);
 }
 
 
